e1bdc0f7ceecf4ea3262017d77242f9e0de7d8cd
Federico Allegrini <federicoa@mondoconvenienza.net>
2020-07-24 11:10:07 +0200

    Routing in server deployment, server redirect 404, basename

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Blog from "./containers/Blog/Blog";
 class App extends Component {
   render() {
     return (
+      // For production set redirect of 404 pages to index.html
+      // This because all routes are inside the react routes in index.html!
+      // Server doesn't know the other routes
+
+      // If you use a sub directory in production server use "basename" for set basepath
+      // <BrowserRouter basename="/my-app">
       <BrowserRouter>
         <div className="App">
           <Blog />
